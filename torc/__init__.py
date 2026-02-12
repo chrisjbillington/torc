9fbf41d8e8e2ea2c9037c36f30de88d0bae78bdf
chrisjbillington <chrisjbillington@gmail.com>
2026-02-12 18:01:13 +1100

    switch to pyproject.toml

--- torc/__init__.py
+++ torc/__init__.py
@@ -1,10 +1,6 @@
-try:
-    from .__version__ import __version__
-except ImportError:
-    __version__ = None
+from torc.__version__ import __version__
 
-
-from .torc import (
+from torc.torc import (
     COPPER,
     SILVER,
     mm,
@@ -26,26 +22,3 @@ from .torc import (
     RacetrackCoil,
     CoilPair,
 )
-
-__all__ = [
-    'COPPER',
-    'SILVER',
-    'mm',
-    'inch',
-    'cm',
-    'gauss',
-    'gauss_per_cm',
-    'X',
-    'Y',
-    'Z',
-    'CurrentObject',
-    'Container',
-    'Line',
-    'Arc',
-    'Loop',
-    'StraightSegment',
-    'CurvedSegment',
-    'RoundCoil',
-    'RacetrackCoil',
-    'CoilPair',
-]
